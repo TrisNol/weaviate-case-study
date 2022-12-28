43099538d0356d23768cb2311dca574155392818
TrisNol <tristan.nolde@yahoo.de>
2022-12-28 17:00:11 +0100

    Introduce basis unit test for dataset download

---
+++ test/test_main.py
@@ -0,0 +1,10 @@
+import os
+import unittest
+from app.main import downdload_txt_to_file
+
+class MainTest(unittest.TestCase):
+
+    def test_download_txt_to_file(self):
+        downdload_txt_to_file("https://raw.githubusercontent.com/TrisNol/sample-python-repo/main/requirements.txt", "./temp.txt")
+
+        self.assertTrue(os.path.exists("./temp.txt"))
